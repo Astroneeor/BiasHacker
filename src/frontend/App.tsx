ed4f92dd102f02ed0e8d137d51d3093ebc32e3c2
Gooogllleee <129225137+reyanshBadhwar26@users.noreply.github.com>
2025-03-23 00:47:08 -0600

    Header Added Back

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import Header from './components/Header';
 import DoctorDashboard from './pages/DoctorDashboard';
 import './App.css';
 import 'spectre.css/dist/spectre.min.css';
@@ -10,12 +11,15 @@ import PatientSurvey from './pages/PatientSurvey';
 function App() {
   return (
     <Router>
+      <div className = "App">
+        <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/patient" element={<PatientSurvey />} />
         <Route path="/patient/success" element={<PatientSuccess />} />
         <Route path="/doctor" element={<DoctorDashboard />} />
       </Routes>
+      </div>
     </Router>
   );
 }
