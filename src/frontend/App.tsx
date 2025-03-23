285572a0b04dcebc9197a9f4f0bf3d6ee7a39ab7
fabf28 <ifavour228@gmail.com>
2025-03-22 19:12:20 -0600

    app.tsx change

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,17 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
-import PatientSurvey from './pages/Survey';
 import DoctorDashboard from './pages/DoctorDashboard';
 import './App.css';
 import 'spectre.css/dist/spectre.min.css';
+import PatientSuccess from './pages/PatientSuccess';
+import PatientSurvey from './pages/PatientSurvey';
 
-function PatientSurvey() {
-  return <h2>Patient Survey Page</h2>; // placeholder
-}
-
-function DoctorDashboard() {
-  return <h2>Doctor Dashboard</h2>; // placeholder
-}
 
 function App() {
   return (
@@ -19,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/patient" element={<PatientSurvey />} />
-        <Route path="/patient/success" element={<PatientCode />} />
+        <Route path="/patient/success" element={<PatientSuccess />} />
         <Route path="/doctor" element={<DoctorDashboard />} />
       </Routes>
     </Router>
