8e1e0b4bab4fcadd34ff8756cf27d7cc8579f0e7
fabf28 <ifavour228@gmail.com>
2025-03-22 19:02:10 -0600

    patient data formatted for submission

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -3,10 +3,9 @@ import Homepage from './pages/Homepage';
 import DoctorLogin from './pages/DoctorLogin';
 import './App.css';
 import 'spectre.css/dist/spectre.min.css';
+import PatientSurvey from './pages/PatientSurvey';
+import PatientCode from './pages/PatientSuccess';
 
-function PatientSurvey() {
-  return <h2>Patient Survey Page</h2>; // placeholder
-}
 
 function DoctorDashboard() {
   return <h2>Doctor Dashboard</h2>; // placeholder
@@ -18,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/patient" element={<PatientSurvey />} />
+        <Route path="/patient/success" element={<PatientCode />} />
         <Route path="/doctor" element={<DoctorLogin />} />
         <Route path="/dashboard" element={<DoctorDashboard />} />
       </Routes>
